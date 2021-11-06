6a4d42bdaf6e28b3bf338128e740974187fe3643
nylcheung <nylcheung2000@gmail.com>
2021-11-05 18:58:38 -0700

    Implemented View.onclicklistener and OnClick method

--- app/src/main/java/com/example/group4_finalproject/MainActivity.java
+++ app/src/main/java/com/example/group4_finalproject/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.group4_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 
     //hello, how are you, I am underwater now HUhuhuHUHUHUHUH

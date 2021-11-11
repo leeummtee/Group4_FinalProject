ab6fed13bc1a90d750649dd2ba9b638d9b1cccab
leeummtee <leeummtee@gmail.com>
2021-11-10 21:59:46 -0800

    Updated MainActivity.java

--- app/src/main/java/com/example/group4_finalproject/MainActivity.java
+++ app/src/main/java/com/example/group4_finalproject/MainActivity.java
@@ -2,10 +2,18 @@ package com.example.group4_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+//    https://www.youtube.com/watch?v=NnvJylicKvE&ab_channel=SarthiTechnology
+    private TextView textViewStepCounter, textViewStepDetector;
+    private SensorManager sensorManger;
+    private Sensor myStepCounter;
+    private boolean isCounterSensorPresent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

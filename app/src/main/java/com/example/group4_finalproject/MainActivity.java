5f228592d17d69ed1096d737b44143b56c11a580
leeummtee <leeummtee@gmail.com>
2021-11-11 16:32:19 -0800

    tried getting the StepCounter to work
    Edited MainActivity.java & tracking_page.xml

--- app/src/main/java/com/example/group4_finalproject/MainActivity.java
+++ app/src/main/java/com/example/group4_finalproject/MainActivity.java
@@ -3,26 +3,73 @@ package com.example.group4_finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
 //    https://www.youtube.com/watch?v=NnvJylicKvE&ab_channel=SarthiTechnology
     private TextView textViewStepCounter, textViewStepDetector;
-    private SensorManager sensorManger;
-    private Sensor myStepCounter;
+    private SensorManager sensorManager;
+    private Sensor mStepCounter;
     private boolean isCounterSensorPresent;
+    int stepCount = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tutorial_1);
+        setContentView(R.layout.tracking_page);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        textViewStepCounter = findViewById(R.id.stepCounterTextView);
+//        textViewStepCounter = findViewById(R.id.textViewStepDetector);
+
+        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) !=null)
+        {
+            mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+            isCounterSensorPresent = true;
+        } else {
+            textViewStepCounter.setText("Counter Sensor is not Present!");
+            isCounterSensorPresent = false;
+        }
     }
 
     @Override
     public void onClick(View view) {
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if(sensorEvent.sensor == mStepCounter) {
+            stepCount = (int)sensorEvent.values[0];
+            textViewStepCounter.setText(String.valueOf(stepCount));
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) !=null)
+            sensorManager.registerListener(this, mStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     //hello, how are you, I am underwater now HUhuhuHUHUHUHUH

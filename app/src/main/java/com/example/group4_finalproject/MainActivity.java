27bdbdb6ca93092cad82b6791b2aa024e2f1f71f
leeummtee <leeummtee@gmail.com>
2021-11-12 18:32:23 -0800

    Finished the basic step counter
    Edited MainActivity.java and tracking_page.xml

--- app/src/main/java/com/example/group4_finalproject/MainActivity.java
+++ app/src/main/java/com/example/group4_finalproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.group4_finalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -12,64 +13,61 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
-//    https://www.youtube.com/watch?v=NnvJylicKvE&ab_channel=SarthiTechnology
-    private TextView textViewStepCounter, textViewStepDetector;
-    private SensorManager sensorManager;
-    private Sensor mStepCounter;
-    private boolean isCounterSensorPresent;
-    int stepCount = 0;
-
+public class MainActivity extends AppCompatActivity {
+//    https://www.youtube.com/watch?v=o-qpVefrfVA&ab_channel=ProgrammerWorld
+    private TextView textViewStepCounter;
+    private double MagnitudePrevious = 0;
+    private Integer stepCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tracking_page);
 
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         textViewStepCounter = findViewById(R.id.stepCounterTextView);
-//        textViewStepCounter = findViewById(R.id.textViewStepDetector);
+        SensorManager sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        SensorEventListener stepDetector = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent != null) {
+                    float x_acceleration = sensorEvent.values[0];
+                    float y_acceleration = sensorEvent.values[1];
+                    float z_acceleration = sensorEvent.values[2];
 
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) !=null)
-        {
-            mStepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-            isCounterSensorPresent = true;
-        } else {
-            textViewStepCounter.setText("Counter Sensor is not Present!");
-            isCounterSensorPresent = false;
-        }
+                    double Magnitude = Math.sqrt(x_acceleration*x_acceleration + y_acceleration*y_acceleration + z_acceleration*z_acceleration);
+                    double MagnitudeDelta = Magnitude - MagnitudePrevious;
+                    MagnitudePrevious = Magnitude;
+
+                    if(MagnitudeDelta > 1) {
+                        stepCount++;
+                    }
+                    textViewStepCounter.setText(stepCount.toString());
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+        sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    @Override
-    public void onClick(View view) {
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if(sensorEvent.sensor == mStepCounter) {
-            stepCount = (int)sensorEvent.values[0];
-            textViewStepCounter.setText(String.valueOf(stepCount));
-        }
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        if(sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) !=null)
-            sensorManager.registerListener(this, mStepCounter, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
     protected void onPause() {
         super.onPause();
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.putInt("stepCount", stepCount);
+        editor.apply();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = getPreferences(MODE_PRIVATE);
+        stepCount = sharedPreferences.getInt("stepCount", 0);
     }
 
     //hello, how are you, I am underwater now HUhuhuHUHUHUHUH

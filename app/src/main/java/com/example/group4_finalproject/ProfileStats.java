473059c3c76db9c4f998b0ac5ca69c457f7dd76f
nylcheung <nylcheung2000@gmail.com>
2021-11-17 13:21:07 -0800

    made a java class for the profile stats

---
+++ app/src/main/java/com/example/group4_finalproject/ProfileStats.java
@@ -0,0 +1,12 @@
+package com.example.group4_finalproject;
+
+import android.os.Bundle;
+
+public class ProfileStats extends MainActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_stats);
+    }
+}

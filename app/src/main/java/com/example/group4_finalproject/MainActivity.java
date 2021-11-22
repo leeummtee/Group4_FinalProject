02142227ef367c9ecc11b332763f527a12649e6d
nylcheung <nylcheung2000@gmail.com>
2021-11-22 14:48:57 -0800

    moved the camera

--- app/src/main/java/com/example/group4_finalproject/MainActivity.java
+++ app/src/main/java/com/example/group4_finalproject/MainActivity.java
@@ -115,11 +115,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         sensorManager.registerListener(stepDetector, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng surreyCentral = new LatLng(50,50);
         googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
+                .position(surreyCentral)
                 .title("Marker"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(surreyCentral));
     }
 
     protected void onPause() {
@@ -178,78 +181,4 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
-
-
-    //    //https://stackoverflow.com/questions/19353255/how-to-put-google-maps-v2-on-a-fragment-using-viewpager
-//    public class MapFragment extends Fragment implements OnMapReadyCallback {
-//        MapView mMapView;
-//        private GoogleMap googleMap;
-//
-//        @Override
-//        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-//            View rootView = inflater.inflate(R.layout.tracking_page, container, false);
-//
-//            mMapView = (MapView) rootView.findViewById(R.id.mapView);
-//            mMapView.onCreate(savedInstanceState);
-//
-//            mMapView.onResume(); // needed to get the map to display immediately
-//
-//            try {
-//                MapsInitializer.initialize(getActivity().getApplicationContext());
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-//
-//            mMapView.getMapAsync(new OnMapReadyCallback() {
-//
-//                @Override
-//                public void onMapReady(GoogleMap mMap) {
-//                    googleMap = mMap;
-//
-//                    // For showing a move to my location button
-//
-//                    googleMap.setMyLocationEnabled(true);
-//
-//                    // For dropping a marker at a point on the Map
-//                    LatLng sydney = new LatLng(-49, 122);
-//                    googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
-//
-//                    // For zooming automatically to the location of the marker
-//                    CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
-//                    googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-//                }
-//            });
-//
-//            return rootView;
-//        }
-//
-//        @Override
-//        public void onResume() {
-//            super.onResume();
-//            mMapView.onResume();
-//        }
-//
-//        @Override
-//        public void onPause() {
-//            super.onPause();
-//            mMapView.onPause();
-//        }
-//
-//        @Override
-//        public void onDestroy() {
-//            super.onDestroy();
-//            mMapView.onDestroy();
-//        }
-//
-//        @Override
-//        public void onLowMemory() {
-//            super.onLowMemory();
-//            mMapView.onLowMemory();
-//        }
-//
-//        @Override
-//        public void onMapReady(@NonNull GoogleMap googleMap) {
-//
-//        }
-//    }
 }
